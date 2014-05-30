943c23190d590b0af80f807a95804ab8c2861526
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2014-05-29 23:14:03 -0400

    first stab at program editor

--- cud.py
+++ cud.py
@@ -5,6 +5,10 @@ import string
 import cherrypy
 import telnetlib
 
+import threading
+
+fetch_lock = threading.Lock()
+
 class CudIndex(object):
 
     @cherrypy.expose
@@ -27,11 +31,36 @@ class CudInput(object):
         print 'input >>{0}<<'.format(input_string)
         cherrypy.session['server'].write('{0}\n'.format(input_string))
 
+class CudFetchVerb(object):
+    exposed = True
+
+    def POST(self, verb_text):
+        global fetch_lock
+        fetch_lock.acquire()
+        print 'fetching verb {0}'.format(verb_text)
+        cherrypy.session['server'].write('@list {0}\n'.format(verb_text))
+        val=''
+        while not val:
+            val=cherrypy.session['server'].read_very_eager()
+        fetch_lock.release()
+        
+        return process_verb_code(val)
+        
+class CudProgram(object):
+    exposed = True
+
+    def PUT(self, input_verb, input_program):
+        print 'programming...'
+        cherrypy.session['server'].write('@program {0}\n'.format(input_verb))
+        cherrypy.session['server'].write('{0}\n.\n'.format(input_program))
+
 class CudLog(object):
     exposed = True
     def GET(self):
-        print 'getting...'
+        global fetch_lock
+        fetch_lock.acquire()
         val=''
+        print 'getting...'
         try:
             val=cherrypy.session['server'].read_very_eager()
             if val:
@@ -40,6 +69,7 @@ class CudLog(object):
             print 'CLOSED!'
             val = '%%%'
             cherrypy.session['server'].close()
+        fetch_lock.release()
         return val
 
 class CudKill(object):
@@ -47,6 +77,21 @@ class CudKill(object):
     def PUT(self):
         print 'KILLED!'
         cherrypy.session['server'].close()
+
+def process_verb_code(the_code):
+    # drop the first line
+    lines = the_code.split('\n')
+    lines = lines[1:]
+    
+    newlines=[]
+    for line in lines:
+        tmp=line.split(':',1)
+        if len(tmp) > 1:
+            newlines.append(tmp[1])
+
+    new_code = '\n'.join(newlines)
+    print '\n\n'+new_code+'\n\n'
+    return new_code
 
 if __name__ == '__main__':
 
@@ -74,6 +119,24 @@ if __name__ == '__main__':
 
     cherrypy.tree.mount(
         CudInput(), '/input',
+        {'/': {
+                'tools.sessions.on': True,        
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+                }
+        }
+    )
+
+    cherrypy.tree.mount(
+        CudFetchVerb(), '/fetchverb',
+        {'/': {
+                'tools.sessions.on': True,        
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+                }
+        }
+    )
+
+    cherrypy.tree.mount(
+        CudProgram(), '/program',
         {'/': {
                 'tools.sessions.on': True,        
                 'request.dispatch': cherrypy.dispatch.MethodDispatcher()
