6a79c0142f75d07e4fa8a310a01bf9169637ce2a
aaronoppenheimer <aoppenheimer@gmail.com>
2014-06-03 10:18:27 +0000

    updated to allow use on ec2

--- cud.py
+++ cud.py
@@ -170,6 +170,10 @@ if __name__ == '__main__':
 
     cherrypy.log.screen = False
 
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': 8080
+                        })
+
     cherrypy.engine.start()
     cherrypy.engine.block()
     
